22cc6eedeef589724385ee79a6e41e50b7f71538
The-Napster <connorh16@gmail.com>
2020-08-26 22:51:04 -0400

    Working navbar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import Nav from "./Components/Nav"
+import Top from "./Components/Nav";
 
 
 
 class App extends Component {
   render() {
     return (
-      <Nav />
+      <Top />
     );
   }
 }
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
+  const div = document.createElement("p");
+  div.innerHTML("hello");
   ReactDOM.render(<App />, div);
 });
