fb2c054daec17f3e6b3538ef471ffe8f5927ff1b
The-Napster <connorh16@gmail.com>
2020-08-26 22:07:28 -0400

    added react bootstrap

---
+++ client/src/Components/Nav/index.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import { Navbar } from "react-bootstrap";
+
+function Nav() {
+    return (
+        <Navbar bg="danger">
+            <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+        </Navbar>
+    )
+}
+
+
+
+export default Nav
