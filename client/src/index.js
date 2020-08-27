15e06b78e474d68d0ceda1290778b04e8cdc70c0
The-Napster <connorh16@gmail.com>
2020-08-26 22:16:22 -0400

    added script tags

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <App />, document.getElementById("root"));
+
 registerServiceWorker();
