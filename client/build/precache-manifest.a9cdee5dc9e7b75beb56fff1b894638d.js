35d850a1f31d4f955c0e2caecb243cee3674dc60
The-Napster <connorh16@gmail.com>
2020-08-26 21:08:58 -0400

    pushing what I have. Basic layout.

---
+++ client/build/precache-manifest.a9cdee5dc9e7b75beb56fff1b894638d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "06f51380d95365364adb",
+    "url": "/static/css/main.a684be82.chunk.css"
+  },
+  {
+    "revision": "06f51380d95365364adb",
+    "url": "/static/js/main.6ab8764d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f5a0adc8d0bc3fbf7243",
+    "url": "/static/js/2.5145a938.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "873313e3467fe59b2a200d22c51b88bc",
+    "url": "/index.html"
+  }
+];
