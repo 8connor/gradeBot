4efd35b9ffc3591b0213b1f68456efbbce8e97c1
The-Napster <connorh16@gmail.com>
2020-09-27 14:12:36 -0400

    fixing files.

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,6 @@ import AuthProvider from "./Context/AuthContext"
 // The entire app will now have access to user and isAuthenticated global states 
 ReactDOM.render(
     <AuthProvider>
-        <
         <App />
     </AuthProvider>,
     document.getElementById('root')
